245d1dac76618ad8ae5f62ef1a6308975e82cd72
hosomikai <kai@twjoin.com>
2019-12-04 20:21:16 +0800

    feat(FormRequest): add send email request

---
+++ src/Http/Requests/ContactForm.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace hosomikai\ContactForm\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContactForm extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone' => 'sometimes|required|size:9',
+            'message' => 'required|min:12',
+        ];
+    }
+
+    /**
+     * Get custom messages for validator errors.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [];
+    }
+}
