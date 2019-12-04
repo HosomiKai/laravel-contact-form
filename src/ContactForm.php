dff21a255ab4ce0db3dae1879f756a5c2c8fbfde
hosomikai <kai@twjoin.com>
2019-12-04 20:22:51 +0800

    feat(ContactForm): add method to send contact form

--- src/ContactForm.php
+++ src/ContactForm.php
@@ -2,7 +2,21 @@
 
 namespace hosomikai\ContactForm;
 
+use hosomikai\ContactForm\Mail\ContactForm as ContactFormMail;
+use Illuminate\Support\Facades\Mail;
+
 class ContactForm
 {
-    // Build wonderful things
+    /**
+     * 送出聯絡表單.
+     *
+     * @param string $phone
+     *
+     * @return void
+     */
+    public function send(string $name, string $email, string $content, string $phone = null)
+    {
+        Mail::to(config('contact.email'))
+            ->send(new ContactFormMail($name, $email, $content, $phone));
+    }
 }
